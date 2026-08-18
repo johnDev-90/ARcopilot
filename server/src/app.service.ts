055a15eea54bc51f6b527b3f6b5a27d7ed9c2404
Joh Martínez <110352229+johnDev-90@users.noreply.github.com>
2026-08-17 19:42:19 -0700

    Feat: Add docker-compose-yml

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Docker, new container.... '
+    return 'Hello Docker, .here is john '
   }
 }
